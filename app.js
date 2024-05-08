b6788149f0622da4b3dd69ba352e34fc36216644
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-08 23:11:13 +0000

    [update]5章まで

---
+++ app.js
@@ -0,0 +1,11 @@
+$(function (){
+  $('.box1').slideDown(1000,function(){
+    $('.box1').css({
+      'background-color' : '#0000FF',
+      'width' : '200px',
+      'height' : '100px'
+    }).slideUp(1000);
+  });
+});
+
+
