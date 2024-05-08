2b55e97e5b14d161a570828bea24c370991b2860
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-05-08 23:34:55 +0000

    [update]6章

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 $(function (){
-  $('.box1').slideDown(1000,function(){
-    $('.box1').css({
-      'background-color' : '#0000FF',
-      'width' : '200px',
-      'height' : '100px'
-    }).slideUp(1000);
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
 
